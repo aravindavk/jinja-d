9dec7706cb273496cac21d3298c8e3d427463ec5
Aravinda VK <vkaravinda7@gmail.com>
2024-09-23 20:56:22 +0530

    Added Text parsing and package code
    Signed-off-by: Aravinda VK <vkaravinda7@gmail.com>

---
+++ source/jinja/package.d
@@ -0,0 +1,58 @@
+module jinja;
+
+import std.file;
+import std.path;
+
+import pegged.grammar;
+import jinja.parser;
+
+struct JinjaSettings
+{
+    string viewsDirectory = "views";
+}
+
+class Jinja
+{
+    JinjaSettings settings;
+
+    this(JinjaSettings settings = JinjaSettings.init)
+    {
+        this.settings = settings;
+    }
+
+    private string parse(ParseTree parsedTmpl)
+    {
+        switch(parsedTmpl.name)
+        {
+        case "JinjaTemplate":
+            return parse(parsedTmpl.children[0]);
+        case "JinjaTemplate.Template":
+            string result = "";
+            foreach(child; parsedTmpl.children)
+                result ~= parse(child);
+            return result;
+        case "JinjaTemplate.Text":
+            return parsedTmpl.matches[0];
+        default:
+            return "";
+        }
+    }
+
+    string render(string tmpl)
+    {
+        auto parsedTmpl = JinjaTemplate(tmpl);
+        return parse(parsedTmpl);
+    }
+
+    string renderFile(string fileName)
+    {
+        auto tmpl = readText(buildPath(settings.viewsDirectory, fileName));
+        return render(tmpl);
+    }
+}
+
+unittest
+{
+    auto view = new Jinja;
+    assert(view.render("Hello World!") == "Hello World!", view.render("Hello World!"));
+}
--- source/jinja/parser.d
+++ source/jinja/parser.d
@@ -6,8 +6,8 @@ import pegged.examples.numbers;
 
 enum jinjaTemplateGrammer = `
 JinjaTemplate:
-  Template           <- Text+
-  Text               <- (!OpenInterpolation !OpenStatement !OpenComment .)+
+  Template           <- Text
+  Text               <~ (!(OpenInterpolation / OpenStatement / OpenComment) .)+
   OpenInterpolation  <- "{{"
   CloseInterpolation <- "}}"
   OpenStatement      <- "{%"
@@ -21,8 +21,8 @@ mixin(grammar(jinjaTemplateGrammer));
 unittest
 {
     auto tmpl = JinjaTemplate("Hello World!");
-    assert(tmpl.toString == `JinjaTemplate[0, 12]["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d", "!"]
- +-JinjaTemplate.Template[0, 12]["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d", "!"]
-    +-JinjaTemplate.Text[0, 12]["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d", "!"]
+    assert(tmpl.toString == `JinjaTemplate[0, 12]["Hello World!"]
+ +-JinjaTemplate.Template[0, 12]["Hello World!"]
+    +-JinjaTemplate.Text[0, 12]["Hello World!"]
 `);
 }
